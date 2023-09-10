98d1670a12fcbd90d7fa713c485a3e57760cd6a7
dimathecreator <dzhosan777@gmail.com>
2023-09-10 19:46:36 +0200

    new

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -29,25 +29,28 @@ public:
         char buffHomeNum[50];
         char buffMobileNum[50];
         
-        name = new char[strlen(buffName) + 1];
-        vorname = new char[strlen(buffVorname) + 1];
-        homeNum = new char[strlen(buffHomeNum) + 1];
-        mobileNum = new char[strlen(buffMobileNum) + 1];
+     
+
+        
         
         cout << "enter a name: ";
         cin >> buffName;
+        name = new char[strlen(buffName) + 1];
         strcpy(name, buffName);
         
         cout << "enter a vorname: ";
         cin >> buffVorname;
+        vorname = new char[strlen(buffVorname) + 1];
         strcpy(vorname, buffVorname);
         
         cout << "enter a home number: ";
         cin >> buffHomeNum;
+        homeNum = new char[strlen(buffHomeNum) + 1];
         strcpy(homeNum, buffHomeNum);
         
         cout << "enter a mobile number: ";
         cin >> buffMobileNum;
+        mobileNum = new char[strlen(buffMobileNum) + 1];
         strcpy(mobileNum, buffMobileNum);
         
     }
@@ -55,13 +58,7 @@ public:
         return vorname;
     }
     
-    ~Book(){//деструктор
-        delete[] name;
-        delete[] vorname;
-        delete[] homeNum;
-        delete[] mobileNum;
-    }
-    
+   
     
     void print(Book* users, int size){
         for(int i = 0; i < size; i++){
@@ -76,58 +73,103 @@ public:
             users -> input();
         }
     }
-
     
-    void deleteUser(Book* users, int size){
-        char buff[100];
+    void search(Book* users, int size){
+        char buffer[100];
+        cout << "введите фамилию для поиска: ";
+        cin >> buffer;
         int index = 0;
-        cout << "введите фамилию пользователя, которого хотите удалить: ";
-        cin >> buff;
-        Book* new_users = new Book[size - 1];
         for(int i = 0; i < size; i++){
-            if(strcmp(buff, users[i].vorname) == 0){
+            if(strcmp(buffer, users[i].vorname) == 0){
                 index = i;
                 break;
-        
-        }
-       
-    }
-        int k = 0;
-        for(int i = 0; i < size - 1; i++){
-            if(index != i){
-                new_users[i] = users[k];
-                k++;
             }
+        }
+        
+            cout << users[index].name << endl;
+            cout << users[index].vorname << endl;
+            cout << users[index].homeNum << endl;
+            cout << users[index].mobileNum << endl;
+        
+    }
+    
+    void save(FILE* file, Book* users, int size){
+        file = fopen("txt", "w");
+        if(file == 0){
+            cout << "error" << endl;
+        }
+        else{
+            for (int i = 0; i < size; i++) {
+                fputs(users[i].name, file);
+                fputs(users[i].vorname, file);
+                fputs(users[i].homeNum, file);
+                fputs(users[i].mobileNum, file);
+            }
+            cout << "данные записаны успешно!" << endl;
+            fclose(file);
         }
     }
     
-   
-  
+    void read(FILE* file, Book* users, int size){
+        file = fopen("txt", "r");
+        if(file == 0){
+            cout << "error" << endl;
+        }
+        else{
+            char buffer[100];
+            while (!feof(file)) {
+                fgets(buffer, 100, file);
+            }
+            fclose(file);
+            cout << buffer << endl;
+
+            
+        }
+        
+    }
+
+    
+    
+    
+    void deleteUser(Book* users, int size){
+        char buffer[100];
+        cout << "введите фамилию, которую хотите удалить: ";
+        cin >> buffer;
+        int index;
+        for(int i = 0; i < size; i++){
+            if(strcmp(users[i].vorname, buffer) == 0){
+                index = i;
+                break;
+            }
+    }
+        int newSize = size - 1;
+        Book* newUsers = new Book[newSize];
+        int k = 0;
+        for(int i = 0; i < newSize; i++){
+            newUsers[k].name = users[i].name;
+            newUsers[k].vorname = users[i].vorname;
+            newUsers[k].homeNum = users[i].homeNum;
+            newUsers[k].mobileNum = users[i].mobileNum;
+            k++;
+        }
+    }
+
+ 
+    ~Book(){//деструктор
+        delete[] name;
+        delete[] vorname;
+        delete[] homeNum;
+        delete[] mobileNum;
+    }
+    
 };
 
 
 
-//void deleteUser(Book* users, int size){
-//    char buff[100];
-//    int index = 0;
-//    cout << "введите фамилию пользователя, которого хотите удалить: ";
-//    cin >> buff;
-//    Book* new_users = new Book[size - 1];
-//    for(int i = 0; i < size; i++){
-//   char* buffVorname = users[i].getVorname();
-//        if(strcmp(buff, buffVorname) == 0){
-//            index = i;
-//            break;
-//
-//    }
-//
-//}
-//    for(int i = 0; i < size - 1; i++){
-//
-//    }
-//}
+
 
 int main() {
+    FILE* file;
     int size = 1;
     Book* users = new Book[size];
     int answer;
@@ -140,8 +182,32 @@ int main() {
         cin >> size;
         users -> addUser(users, size);
     }
-//    deleteUser(users, size);
-    users -> deleteUser(users, size);
-    users -> print(users, size - 1);
+    int answer1;
+    cout << "выберите действие: ";
+    cout << "1 - осуществить поиск по фамилии" << endl;
+    cout << "2 - удалить пользователя" << endl;
+    cout << "3 - сохранить данные в файл" << endl;
+    cout << "4 - считать данные из файла" << endl;
+    cin >> answer1;
+    
+    switch(answer1){
+        case 1:
+            users -> search(users, size);
+            break;
+            
+        case 2:
+                users -> deleteUser(users, size);
+            break;
+            
+        case 3:
+            users -> save(file, users, size);
+            break;
+            
+        case 4:
+            users -> read(file, users, size);
+            break;
+    }
+
     delete[] users;
+
 }
